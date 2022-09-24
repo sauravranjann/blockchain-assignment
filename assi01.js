b2d09b4fa392107348a48acad61da7adf769bb24
sauravranjann <109453979+sauravranjann@users.noreply.github.com>
2022-09-24 22:24:00 +0530

    Add files via upload

---
+++ assi01.js
@@ -0,0 +1,43 @@
+// // 1. Write a program to find whether a given year is a leap year or not.
+
+// let year= 2000;
+// if(year %4 ===0)
+// {
+//     console.log("LEAP YEAR")
+// }
+// else
+// {
+//     console.log("NOT A LEAP YEAR")
+// }
+
+
+
+
+
+
+// // 2. Write a JavaScript program to convert temperatures to and from Celsius,
+// // Fahrenheit.
+// // [ Formula : c/5 = (f-32)/9 [ where c = temperature in Celsius and f = temperature in
+// // Fahrenheit ]
+// // Expected Output :
+// // 60°C is 140 °F
+// // 45°F is 7.222222222222222°C
+
+// let f=140;
+// let c= 5/9 * f-32;
+// console.log(c)
+
+// let c=60;
+// let f= (9/5 *c)+ 32;
+// console.log(f)
+
+
+
+// // 3. Write a program to find the factorial of a number.
+// let j=5;
+// let sum=1;
+// for(let i=1;i<=j;i++)
+// {
+//     sum=sum*i;
+// }
+// console.log(sum);
